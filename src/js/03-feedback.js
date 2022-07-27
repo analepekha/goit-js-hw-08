f665cf67949173b756ab9bae1687db3d8bf7eaae
analepekha <maiboroda0103@gmail.com>
2022-07-28 00:38:24 +0300

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,17 +1,32 @@
 const throttle = require('lodash.throttle');
 
-const form = document.querySelector('.feedback-form')
-const email = document.querySelector('input[name="email"]')
-const message = document.querySelector('textarea[name="message"]')
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
 
-form.addEventListener('submit', onSubmit) 
+const LOCALSTORAGE_KEY = "feedback-form-state";
+const userData = {}
 
-function onSubmit (event) {
+form.addEventListener('input', handleInput)
+
+function handleInput (event) {
+    event.preventDefault()
+    userData[event.target.name] = event.target.value
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(userData));
+}
+
+form.addEventListener('submit', handleSubmit) 
+
+function handleSubmit (event) {
     event.preventDefault()
     const { email, message } = event.target.elements
     if (email.value === "" || message.value === "") {
-        console.log("full fields");
+        alert("Please, fill in all fields");
     }
+    const formData = new FormData(form)
+        formData.forEach((value, name) => { userData[name] = value });
+    console.log(userData);
+    event.currentTarget.reset();
+    // localStorage.removeItem(LOCALSTORAGE_KEY)
 }
 
-console.log("hi");
