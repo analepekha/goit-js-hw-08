f6746811087cf98a69acc155c483e9cb703bdf77
analepekha <maiboroda0103@gmail.com>
2022-07-30 00:23:52 +0300

    bugfix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,9 +5,23 @@ const email = document.querySelector('input[name="email"]');
 const message = document.querySelector('textarea[name="message"]');
 
 const LOCALSTORAGE_KEY = "feedback-form-state";
+
 let userData = {}
 
+const throttleFn = throttle(handleSubmit, 500)
+
 initForm()
+
+form.addEventListener('submit', throttleFn)
+
+function handleSubmit (event) {
+    event.preventDefault()
+    const formData = new FormData(form)
+    formData.forEach((value, name) => { userData[name] = value })
+    console.log(userData);
+    event.currentTarget.reset()
+    localStorage.removeItem(LOCALSTORAGE_KEY)
+}
 
 form.addEventListener('input', handleInput)
 
@@ -16,31 +30,14 @@ function handleInput (event) {
     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(userData));
 }
 
-form.addEventListener('submit', throttle(handleSubmit, 500)) 
-
-function handleSubmit (event) {
-    event.preventDefault()
-    const { email, message } = event.target.elements
-    if (email.value === "" || message.value === "") {
-        alert("Please, fill in all fields");
-    }
-    const formData = new FormData(form)
-    formData.forEach((value, name) => { userData[name] = value });
-    console.log(userData);
-    event.currentTarget.reset();
-    localStorage.removeItem(LOCALSTORAGE_KEY)
-}
-
-function initForm () {
+function initForm() {
     let persistedData = localStorage.getItem(LOCALSTORAGE_KEY)
-    console.log(persistedData);
-    try {
-        persistedData = JSON.parse(LOCALSTORAGE_KEY)
+    if (persistedData) {
+        persistedData = JSON.parse(persistedData)
         Object.entries(persistedData).forEach(([name, value]) => {
             userData[name] = value
             form.elements[name].value = value
         })
-    } catch (error) {
-        console.log(error);
-        }
 }
+}
+
